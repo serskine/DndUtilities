b7fd4155ea4af9777a3bc06947728eb8832aa636
Stuart Marr Erskine <stuart.marr.erskine@gmail.com>
2018-06-10 11:49:12 -0300

    Cleaned up the EntityList DaoModel.

--- app/src/main/java/com/soupthatisthick/encounterbuilder/model/lookup/EntityList.java
+++ app/src/main/java/com/soupthatisthick/encounterbuilder/model/lookup/EntityList.java
@@ -3,38 +3,27 @@ package com.soupthatisthick.encounterbuilder.model.lookup;
 import com.soupthatisthick.encounterbuilder.model.DaoModel;
 import com.soupthatisthick.encounterbuilder.util.sort.Category;
 
-// import org.greenrobot.greendao.annotation.Entity;
-// import org.greenrobot.greendao.annotation.Id;
-// import org.greenrobot.greendao.annotation.Property;
-// import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * Created by Owner on 4/18/2017.
  * Copyright of Stuart Marr Erskine, all rights reserved.
  */
-// @Entity(nameInDb = "LISTS")
 public class EntityList extends DaoModel {
 
-    // @Id
-    // @Property(nameInDb = "id")
     private Long id;
-
-    // @Property(nameInDb = "name")
     private String name;
 
-    // @Generated(hash = 383842011)
     public EntityList(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    // @Generated(hash = 70580157)
     public EntityList() {
+        // Do nothing
     }
 
     /**
      * This returns the unique identifier for the specified list
-     * // @return
      */
     public Long getId() {
         return id;
@@ -42,7 +31,6 @@ public class EntityList extends DaoModel {
 
     /**
      * This sets the id of the specific list
-     * // @param id
      */
     public void setId(Long id) {
         this.id = id;
@@ -50,7 +38,6 @@ public class EntityList extends DaoModel {
 
     /**
      * This will return the name of the list used for display purposes
-     * // @return
      */
     public String getName() {
         return name;
@@ -58,7 +45,6 @@ public class EntityList extends DaoModel {
 
     /**
      * This will set the name of the list used for display purposes.
-     * // @param name
      */
     public void setName(String name) {
         this.name = name;
