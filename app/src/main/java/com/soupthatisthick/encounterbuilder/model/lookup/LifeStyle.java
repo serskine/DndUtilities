dd4777f8e38fc31a5570dc1702bbdcc7cf571db2
Stuart Marr Erskine <stuart.marr.erskine@gmail.com>
2018-06-10 11:59:08 -0300

    Cleaned up the Lifestyle DaoModel.

--- app/src/main/java/com/soupthatisthick/encounterbuilder/model/lookup/LifeStyle.java
+++ app/src/main/java/com/soupthatisthick/encounterbuilder/model/lookup/LifeStyle.java
@@ -2,33 +2,19 @@ package com.soupthatisthick.encounterbuilder.model.lookup;
 
 import com.soupthatisthick.encounterbuilder.model.DaoModel;
 
-// import org.greenrobot.greendao.annotation.Entity;
-// import org.greenrobot.greendao.annotation.Id;
-// import org.greenrobot.greendao.annotation.Property;
-// import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * Created by Owner on 5/5/2017.
  * Copyright of Stuart Marr Erskine, all rights reserved.
  */
 
-// @Entity(nameInDb = "LIFESTYLE")
 public class LifeStyle extends DaoModel {
 
-    // @Id
-    // @Property(nameInDb = "id")
     private Long id;
-
-    // @Property(nameInDb = "name")
     private String name;
-
-    // @Property(nameInDb = "pricePerDay")
     private String pricePerDay;
-
-    // @Property(nameInDb = "description")
     private String description;
 
-    // @Generated(hash = 870436315)
     public LifeStyle(Long id, String name, String pricePerDay, String description) {
         this.id = id;
         this.name = name;
@@ -36,8 +22,8 @@ public class LifeStyle extends DaoModel {
         this.description = description;
     }
 
-    // @Generated(hash = 2041920875)
     public LifeStyle() {
+        // Do nothing
     }
 
     public Long getId() {
