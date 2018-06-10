daa562c4f1533534a36d18efdc1cb1065f7c3387
Stuart Marr Erskine <stuart.marr.erskine@gmail.com>
2018-06-10 11:51:47 -0300

    Cleaned up the Feat DaoModel.

--- app/src/main/java/com/soupthatisthick/encounterbuilder/model/lookup/Feat.java
+++ app/src/main/java/com/soupthatisthick/encounterbuilder/model/lookup/Feat.java
@@ -2,32 +2,18 @@ package com.soupthatisthick.encounterbuilder.model.lookup;
 
 import com.soupthatisthick.encounterbuilder.model.DaoModel;
 
-// import org.greenrobot.greendao.annotation.Entity;
-// import org.greenrobot.greendao.annotation.Id;
-// import org.greenrobot.greendao.annotation.Property;
-// import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * Created by Owner on 5/5/2017.
  * Copyright of Stuart Marr Erskine, all rights reserved.
  */
-// @Entity(nameInDb = "FEATS")
 public class Feat extends DaoModel {
 
-    // @Id
-    // @Property(nameInDb = "id")
     private Long id;
-
-    // @Property(nameInDb = "name")
     private String name;
-
-    // @Property(nameInDb = "prerequisite")
     private String prerequisite;
-
-    // @Property(nameInDb = "description")
     private String description;
 
-    // @Generated(hash = 57394742)
     public Feat(Long id, String name, String prerequisite, String description) {
         this.id = id;
         this.name = name;
@@ -35,8 +21,8 @@ public class Feat extends DaoModel {
         this.description = description;
     }
 
-    // @Generated(hash = 1953217192)
     public Feat() {
+        // Do nothing
     }
 
     public Long getId() {
